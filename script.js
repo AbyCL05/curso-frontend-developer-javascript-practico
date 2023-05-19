5cfe1ef5ce0c05e6377fe2ab0bc0065fb594f1b5
Abigail Chávez <chavezlozanoabigail@gmail.com>
2023-05-18 20:42:42 -0500

    Carrito de compras y menu mobile- ajuste en vista mobile

--- script.js
+++ script.js
@@ -2,14 +2,29 @@ const menuEmail = document.querySelector(".navbar-email");
 const menuDesktop = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const menuMobile = document.querySelector(".mobile-menu");
+const menuCarIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarIcon.addEventListener("click", toggleCarAside);
 
 function toggleDesktopMenu () {
     menuDesktop.classList.toggle("inactive")
 }
 
 function toggleMobileMenu () {
+    const isAsideClosed = aside.classList.contains(".inactive")
+    if (!isAsideClosed) {
+        aside.classList.add("inactive")
+    }
     menuMobile.classList.toggle("inactive")
+}
+
+function toggleCarAside () {
+    const isMobileMenuClosed = menuMobile.classList.contains("inactive")
+    if (!isMobileMenuClosed) {
+        menuMobile.classList.add("inactive")
+    }
+    aside.classList.toggle("inactive")
 }
